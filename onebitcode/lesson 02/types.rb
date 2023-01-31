9cf60d4f48e8f307ac559835475b3fd6a8efbb39
jonathancsr <jonathancsrr@gmail.com>
2023-01-31 17:28:38 -0300

    chore: lesson 02

---
+++ onebitcode/lesson 02/types.rb
@@ -0,0 +1,23 @@
+var = 1
+p var.class
+
+var = 1.1
+p var.class
+
+var = true
+p var.class
+
+var = 'Hello world!'
+p var.class
+
+var = ['alow', 'testando']
+p var.class
+
+var = :teste
+p var.class
+p var.object_id
+
+var = {course: 'ruby', language: 'pt'}
+p var.class
+p var.object_id
+
